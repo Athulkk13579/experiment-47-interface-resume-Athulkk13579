b40317cfe1279cd4ce0d04067993ecf5b556a351
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 16:14:38 +0530

    Update InterfaceResume.java

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,22 +1,33 @@
-import java.util.Scanner;
-
-interface Resume {
-    // TODO: Declare method void biodata();
+abstract class Shape {
+    abstract void numberOfSides();
 }
 
-class Teacher implements Resume {
-    // Attributes
-   
-
-    // TODO: Implement biodata() method to print details in the required format
+class Rectangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Rectangle has 4 sides");
+    }
 }
 
-public class InterfaceResume {
-   
-        
-        // TODO: Read input (Name, Qualification, Experience)
-        // Assign to teacher object attributes
-        
-        // TODO: Call teacher.biodata()
-    
+class Triangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Triangle has 3 sides");
+    }
+}
+
+class Hexagon extends Shape {
+    void numberOfSides() {
+        System.out.println("Hexagon has 6 sides");
+    }
+}
+
+public class AbstractShapes {
+    public static void main(String[] args) {
+        Shape r = new Rectangle();
+        Shape t = new Triangle();
+        Shape h = new Hexagon();
+
+        r.numberOfSides();
+        t.numberOfSides();
+        h.numberOfSides();
+    }
 }
