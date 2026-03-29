ba44404f9cfa20968bfc721332208eaeb75c69cd
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-30 00:06:05 +0530

    Implement Resume interface and biodata method

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
-interface Resume {
-    // TODO: Declare method void biodata();
+interface Resume 
+{
+    void biodata();
 }
 
-class Teacher implements Resume {
-    // Attributes
-   
-
-    // TODO: Implement biodata() method to print details in the required format
+class Teacher implements Resume 
+{
+    String name;
+    String qualification;
+    int experience;
+    public void biodata() 
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Qualification: " + qualification);
+        System.out.println("Experience: " + experience + " years");
+    }
 }
 
-public class InterfaceResume {
-   
-        
-        // TODO: Read input (Name, Qualification, Experience)
-        // Assign to teacher object attributes
-        
-        // TODO: Call teacher.biodata()
-    
+public class InterfaceResume 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        Teacher teacher = new Teacher();
+        teacher.name = sc.nextLine();
+        teacher.qualification = sc.nextLine();
+        teacher.experience = sc.nextInt();
+        teacher.biodata();
+        sc.close();
+    }
 }
